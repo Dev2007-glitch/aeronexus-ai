cf16e132cc4da004122b46a51904441e7276da1d
Dev2007-glitch <deviraghuveerabaib@gmail.com>
2026-06-02 21:40:05 +0530

    added vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -14,16 +14,6 @@
 //   },
 // });
 
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-
-  plugins: [react()],
-
-server: {
-  port: 8080,
-}
-
-});
+export default defineConfig({});
