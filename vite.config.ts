156b591d3221c4bcc0c7db2b1b050c1b5bc0536e
Dev2007-glitch <deviraghuveerabaib@gmail.com>
2026-06-02 21:45:40 +0530

    fixed vercel output

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,16 @@
 //   },
 // });
 
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+
+  plugins: [react()],
+
+server: {
+  port: 8080,
+}
+
+});
